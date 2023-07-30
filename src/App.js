87f1bcce79b7153b7c925071d558ea9963bcbb0a
Michael Robards <mikerobards@icloud.com>
2023-07-30 08:08:01 -0400

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './components/routes/home/home.component';
+import Navbar from './components/navbar/navbar.component';
+import SignIn from './components/routes/sign-in/sign-in.component';
 
-const Navbar = () => {
-  return (
-    <div>
-      <div>
-        <h1>
-          Navbar
-        </h1>
-      </div>
-      <Outlet />
-    </div>
-  )
-}
 
 const Shop = () => {
-  return <h1>Shop</h1>
+  return <h1>This is the Shop</h1>
 }
 
 const App = () => {
@@ -25,6 +15,7 @@ const App = () => {
       <Route path="/" element={<Navbar />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+import './navbar.styles.scss';
+
+const Navbar = () => {
+    return (
+        <Fragment>
+            <div className='navbar'>
+                <Link className='logo-container' to='/'>
+                    <CrownLogo className='logo' />
+                </Link>
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to='/shop'>Shop</Link>
+                    <Link className='nav-link' to='/sign-in'>Sign In</Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default Navbar;
