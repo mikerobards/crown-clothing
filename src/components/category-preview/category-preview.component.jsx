5523e1a4bfd5e2884a1206089f8161a3930025f5
Michael Robards <mikerobards@icloud.com>
2023-08-11 18:53:22 -0400

    start category-preview

---
+++ src/components/category-preview/category-preview.component.jsx
@@ -0,0 +1,6 @@
+import './category-preview.styles.scss';
+
+const CategoryPreview = () => {
+}
+
+export default CategoryPreview;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -16,6 +16,8 @@ import {
     setDoc,
     collection,
     writeBatch,
+    query,
+    getDocs,
 } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -53,6 +55,21 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
     await batch.commit();
     console.log('Collection added');
 };
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnap) => {
+        const { title, items } = docSnap.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {});
+
+    return categoryMap;
+};
+
 
 export const createUserDocumentFromAuth = async (userAuth, addInfo = {}) => {
     if (!userAuth) return;
