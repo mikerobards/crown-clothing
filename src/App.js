c9b09ad2e0106923ef5b0844d7e0828435b1bf79
Michael Robards <mikerobards@icloud.com>
2023-08-24 14:27:05 -0400

    add user action

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux";
+
 import { Routes, Route } from 'react-router-dom';
+
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from "./utils/firebase/firebase.utils";
 
 import Home from './components/routes/home/home.component';
 import Navbar from './components/navbar/navbar.component';
 import Authentication from './components/routes/authentication/authentication.component';
 import Shop from './components/routes/shop/shop.component';
 import Checkout from './components/routes/checkout/checkout.component';
+import { setCurrentUser } from "./store/user/user.action";
 
 
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route path="/" element={<Navbar />}>
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,12 @@
-import { compose, createStore, applyMiddleware } from 'redux';
+import { compose, legacy_createStore, applyMiddleware } from 'redux';
+
 import logger from 'redux-logger';
 
+import { rootReducer } from './root-reducer';
+
+const middleWares = [logger];
+
+const composeEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = legacy_createStore(rootReducer, undefined, composeEnhancers);
 
